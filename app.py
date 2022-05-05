cc7d2161e425fdca69e0a4f9b7f79ef5214c0726
Ítalo Moura <italo.moura1201@gmail.com>
2022-05-04 22:16:04 -0300

    Adptando projeto para receber chamadas do app

--- app.py
+++ app.py
@@ -10,46 +10,46 @@ blueprint = Blueprint('app', __name__, url_prefix='/idrugs-pharma-engine')
 # USER
 @blueprint.route('/user', methods=['POST', 'PUT'])
 def create_user_route():
-    response = request.data
+    response = request.json
     return create_user(response)
 
 @blueprint.route('/user', methods=['GET'])
 def find_user_route():
-    return find_user(request.data)
+    return find_user(request.json)
 
 @blueprint.route('/user', methods=['DELETE'])
 def delete_user_route():
-    response = request.data
+    response = request.json
     return delete_user(response)
 
 # Tipo
 @blueprint.route('/type', methods=['POST', 'PUT'])
 def create_type_route():
-    response = request.data
+    response = request.json
     return create_type(response)
 
 @blueprint.route('/type', methods=['GET'])
 def find_type_route():
-    return find_type(request.data)
+    return find_type(request.json)
 
 @blueprint.route('/type', methods=['DELETE'])
 def delete_type_route():
-    response = request.data
+    response = request.json
     return delete_type(response)
 
 # Produto
 @blueprint.route('/product', methods=['POST', 'PUT'])
 def create_product_route():
-    response = request.data
+    response = request.json
     return create_product(response)
 
 @blueprint.route('/product', methods=['GET'])
 def find_product_route():
-    return find_product(request.data)
+    return find_product(request.json)
 
 @blueprint.route('/product', methods=['DELETE'])
 def delete_product_route():
-    response = request.data
+    response = request.json
     return delete_product(response)
 
 app.register_blueprint(blueprint)
